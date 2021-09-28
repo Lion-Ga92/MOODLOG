1b26d471a996565483aba6bb98da2e67566fdeda
Luis Garcia <luis.ga92forza@gmail.com>
2021-09-28 13:50:35 -0700

    "Newest Changes to file"

--- classes_mood.py
+++ classes_mood.py
@@ -1,7 +1,9 @@
 import sqlite3
+from tkinter import Button, Message
 from guizero import *
-# DB_MAKER IS MOSTLY DONE, IF I COULD ONLY GET IT TO GIVE ME THE SERIAL NUMBER THE 
-# THE WAY I WANT IT THAT WOULD BE GREAT
+from guizero import PushButton
+# DB_MAKER IS DONE, I JUST NEED TO REFACTOR IT TO GET A CLEANER CODE
+
 
 class db_maker:
     def __init__(self):
@@ -13,13 +15,13 @@ class db_maker:
 
         self.cur = self.conn.cursor()
 
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS moodToday ('id' INTEGER, 'moodToday' INTEGER, 'moodTomorrow' INTEGER, 'hygiene' INTEGER, 'spirituality' INTEGER, 'healthEating' INTEGER, 'noGoToWork' INTEGER, 'noworkOut' INTEGER, 'addiction' INTEGER, 'total' INTEGER, 'notes' TEXT); """)
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS moodToday ('date' TEXT, 'moodToday' INTEGER, 'moodTomorrow' INTEGER, 'hygiene' INTEGER, 'spirituality' INTEGER, 'healthEating' INTEGER, 'noGoToWork' INTEGER, 'noworkOut' INTEGER, 'addiction' INTEGER, 'total' INTEGER, 'notes' TEXT); """)
 
         self.conn.commit()
 
         f = 1
         while f != 1:
-            self.conn.close() # I STILL NEED TO FIGURE OUT HOW TO OPEN AND CLOSE THE DB 
+            self.conn.close() # I STILL NEED TO FIGURE OUT HOW TO OPEN AND CLOSE THE DB AS I DONT THINK THIS IS ACCURATE OR FUNCTIONAL 
 
     def db_adder(self, a, b, c, d, e, g, h, i, j, k, m):
         f = 0
@@ -43,32 +45,13 @@ class db_maker:
         self.conn.commit()
         f = 1
 
-    #THIS METHOD ATTEMPTS TO TAKE THE ID VALUE AND ADD ONE TO BE ABLE TO CREATE A SERIALIZED CONTROL SYSTEM
-    #I WAS ABLE TO FIGURE OUT HOW TO TO GET IT TO PRINT THE VALUE QUERIED BUT THAT IS ABOUT IT. 
-    # WHAT I WANT IS TO BE ABLE TO A RANDOM STARTING VALUE AND ADD 1 FROM THERE BUT I THINK A TOTALLY RANDOM 
-    # SERIAL NUMBER WILL BE THE BEST I CAN DO. 
-    def db_id_Selector(self, id_one):
+    # THIS METHOD ATTEMPTS TO TAKE A USER SELECTED VALUE AND RETURN TO THE CONSOLE THE ROW DATA 
+    # REQUESTED IN THE MAIN FILE. THE TROUBLE IS THAT I EITHER GET AN OUTPUT OF NONE FOR THE DATA 
+    # OR RUN INTO A SQLITE OPERATIONAL ERROR 'ROW VALUE MISUSED'
+    def db_id_Selector(self, val):
         f = 0
+        self.val = val
         self.cur = self.conn.cursor()
-        self.cur.execute("""SELECT id FROM moodToday ORDER BY id DESC LIMIT 1;""")
-        last_val = self.cur.fetchone()
-        if id_one != None:
-            last_val += 1
-        
-        id_one = last_val
+        self.cur.execute("""SELECT * FROM moodToday WHERE date = (?, ?, ?, ?, ?, ?, ?, ?, ?);""", self.val)
+        print(self.cur.fetchall())
         f = 1
-
-"""IN ANOTHER CLASS PERHAPS, ENVISION IN MY APP A SMALL NOTEPAD TO BE ABLE 
-    TO WRITE SOME SENTIMENTS DOWN. MAYBE AN OVERLAYER APP THAT SERVES AS THE GUI LOGIN"""
-
-class second_App:
-
-    def __init__(self):
-        self = self
-
-    def second_Wind_app(self):
-        self.second = App(title="Database Entry Display", width=780, height=1000, layout="grid")
-    
-        
-        
-        self.second.display()
--- mood_gui.py
+++ mood_gui.py
@@ -1,17 +1,17 @@
 from genericpath import isfile
 from sys import path
-from tkinter import Spinbox
-from tkinter.constants import X
 from guizero import *
 from classes_mood import *
-import random
 import datetime
+#Gotta clean my import statements
 
 
 databs_obj = db_maker()
 databs_obj.mood_db_setUp()
 
-#This files contains a simple gui using GUIZERO for a mood tracking app. 
+#This files contains a simple gui using GUIZERO for a mood tracking app.
+# PS HOW HARD WILL IT BE TO REFACTOR THIS INTO MAIN FUNCTION? I WANNA START BEING 
+# MORE PEP 8 COMPLIANT  
 
 app = App(title="Luis' Mood Tracker", width=780, height=1000, layout="grid")
 box = Box(app, layout="grid", grid=[0, 0])
@@ -47,6 +47,7 @@ exercise_chckBox = CheckBox(box_2, text="Did not work out", grid=[2, 9])
 addict_checkbox = CheckBox(box_2, text="\nEngaged in Addictive behavior", grid=[2, 10])
 
 notes_txtBox = TextBox(box_3, width=50, height=5, grid=[1, 1], multiline=True, scrollbar=True)
+
 
 #this function serves as the logic of the db_adder it takes the collection of values and 
 # and adds them in their right spot in the db. 
@@ -84,15 +85,10 @@ def submitter(x=1):
 
     total_val = feel_val + tomrrw_val + hygiene_val + spiritual_val + health_eating + work_box_val + exercise_val + addiction_val
     
-    id_val = random.randint(1, 9999999)
 
-    databs_obj.db_adder(id_val, feel_val, tomrrw_val, hygiene_val, spiritual_val, health_eating, work_box_val, exercise_val, addiction_val, total_val, notes_Val)
+    databs_obj.db_adder(date_now, feel_val, tomrrw_val, hygiene_val, spiritual_val, health_eating, work_box_val, exercise_val, addiction_val, total_val, notes_Val)
  
-    # FROM THIS POINT I AM WORKING ON A FILE SYSTEM THAT TAKES A VARIABLE NAME AND STORES IT 
-    # MY HOPE IS TO EVENTUALLY CREATE A SUBDIRECTORY AND TAKE THOSE SAVED FILES AND SERVE THEM BACK
-    # IN A GUIZERO APP TO VIEW READ AND USE.
-    # I ATTEMPTED RECURSION TO CREATE THE MUTABLE FILE NAMES BUT I FEEL I DID IT WRONG 
-    # AND A SIMPLE FOR LOOP ALSO DID NOT WORK
+
 
 
     #MESSAGEBOX WIDGET TODAY
@@ -104,7 +100,44 @@ def submitter(x=1):
     elif mssg_percent >= .50:
         app.info("infoBad", f"Your value for the day is {total_val} out of 45. Please reach out to your support network")
 
+#SOMEHOW WHEN WORKING WITH GUI FRAMEWORKS, THE INTERPRETED NATURE OF LANGUANGE FORCES ME TO CREATE CODE PASTA 
+#BY INSERTING THE FUNC DEFINITION BEFORE I CAN CALL IT. I IMAGINE I COULD TECHNICALLY CALL IT IF I MAKE THE GUI INTO A 
+# SEPARATE FUNCTION INSTEAD OF A RAW DATA. 
+
+def second_Wind_app():
+        second = App(title="Database Entry Display", width=740, height=750, layout="grid")
+        box = Box(second, layout="grid", grid=[0, 0])
+        box2 = Box(second, layout="grid", grid=[0, 1])
+        box3 = Box(second, layout="grid", grid=(0, 2))
+        title = Text(box, text="Data Selection Menu ", grid=[0,0],font="Times New Roman",size=20, color="Green")
+        year_txt = Text(box2, text="Year:\t", grid=[0,1])
+        month_txt = Text(box2, text="Month:\t", grid=[1,1])
+        day_txt = Text(box2, text="Day:\t", grid=[2,1])
+        combo1_yr = Combo(box2, options=["2020", "2021", "2023", "2024"], grid=[0, 2], width=5)
+        combo2_month = Combo(box2, options=["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"], grid=[1, 2], width=5)
+        combo3_day = Combo(box2, options=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"], grid=[2, 2], width=5)
+        displaybx = TextBox(box3, width=50, height=19, grid=[0, 0], multiline=True, scrollbar=True)
+
+       # THIS RIGHT HERE IS THE TROUBLESOME FUNCTION I CANT TELL IF THE COMPARE_DAT  Variable
+       # IS CAUSING AN OPERATIONAL ERROR OR WHAT
+        def button_sub():
+            year_var = (combo1_yr.value)
+            month_var = (combo2_month.value)
+            day_var = (combo3_day.value)
+
+            compare_dat = f'{year_var}-{month_var}-{day_var}'
+            compare_dat = str(compare_dat)
+                        
+            databs_obj.db_id_Selector(compare_dat)
+
+        buttn = PushButton(box3, text="Press here", command=button_sub, grid=[0, 1])
+        
+        second.display()
 
 submit_button = PushButton(box_3, text="Submit" ,command=submitter, width=20, height=1, grid=[1, 2] )
+secondwin_button = PushButton(box_3, text="View Data" ,command=second_Wind_app, width=20, height=1, grid=[1, 3] )
 
 app.display()
+
+
+
